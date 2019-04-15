72470a761a15d0f0b2ece73db1a1b99a1456dd69
PRMcgill <mcgill.patrick2192@gmail.com>
2019-04-15 15:17:12 -0400

    added the api blueprints to the app.py page so we can do the other routes to the api

--- src/app.py
+++ src/app.py
@@ -5,6 +5,8 @@ from .models import db, bcrypt
 from .models import user
 
 from .views.user_view import user_api as user_blueprint
+from .views.pokemon_view import pokemon_api as pokemon_blueprint
+from .views.team_view import team_api as team_blueprint
 from flask_cors import CORS
 
 
@@ -19,6 +21,8 @@ def create_app(env_name='development'):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+    app.register_blueprint(pokemon_blueprint, url_prefix='/api/v1/pokemon')
+    app.register_blueprint(team_blueprint, url_prefix='/api/v1/teams')
     
 
     bcrypt.init_app(app)
