86dccc161618ae27dd87df110e9b7131752b1936
PRMcgill <mcgill.patrick2192@gmail.com>
2019-04-16 16:49:34 -0400

    reverted what i just added

--- src/views/pokemon_view.py
+++ src/views/pokemon_view.py
@@ -1,6 +1,5 @@
 from flask import request, json, Response, Blueprint, g
 from ..models.pokemon import PokemonModel, PokemonSchema
-import pokebase as pb
 
 pokemon_api = Blueprint('pokemon', __name__)
 pokemon_schema = PokemonSchema()
@@ -26,42 +25,3 @@ def get_pokemon(pokemonnumber):
     ser_pokemon = pokemon_schema.dump(pokemon).data
     return custom_response(ser_user, 200)
 
-# @pokemon_api.route('/add', methods=['POST'])
-# def add_all_pokemon():
-#     '''
-#     Generates all the pokemon into the database.
-#     '''
-# for i in range(1, 808):
-        
-#         new_pokemon = pb.pokemon(i)
-#         new_pokemon_species = pb.pokemon_species(i)
-#         pokemon_fte = new_pokemon_species.flavor_text_entries
-#         for fte in pokemon_fte:
-#                 if fte.language.name =="en" and fte.version.name == "alpha-sapphire" or fte.language.name == "en" and fte.version.name == "ultra-sun":
-#                         new_pokemon_description = fte.flavor_text
-#         new_pokemon_name =new_pokemon.name
-#         new_pokemon_number = new_pokemon.id
-#         types = []
-#         type1 = new_pokemon.types[0].type
-#         types.append(type1)
-#         try:
-#                 type2 = new_pokemon.types[1].type
-#         except IndexError:
-#                 type2 = ''
-#         if type2 == '':
-#                 type2 = None
-#                 types.append(type2)
-#         else:
-#                 types.append(type2)
-#         new_pokemon_type1 = types[0]
-#         if len(types) ==2:
-#                 new_pokemon_type2 = types[1]
-#         else:
-#                 pass
-#         new_pokemon_image = new_pokemon.sprites.front_default
-#         new_pokedex_entry = {"name": new_pokemon_name,
-#                 "number": new_pokemon_number,
-#                 "type1": new_pokemon_type1,
-#                 "type2": new_pokemon_type2,
-#                 "image": new_pokemon_image,
-#                 "description": new_pokemon_description}
