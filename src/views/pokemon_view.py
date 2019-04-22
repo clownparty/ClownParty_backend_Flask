7a1094d4488e1e6be2703ff8cf25b882d7d8349e
Molly Hopper <mollyhopper95@gmail.com>
2019-04-22 16:21:40 -0400

    more spelling, etc

--- src/views/pokemon_view.py
+++ src/views/pokemon_view.py
@@ -18,6 +18,19 @@ def get_pokemon(pokemonnumber):
     '''
     Get a single pokemon
     '''
+    pokemon = PokemonModel.get_pokemon_by_name(pokemonnumber)
+    # print(pokemon)
+    if not pokemon:
+        return custom_response({'error': 'Pokemon not found'}, 404)
+
+    ser_pokemon = pokemon_schema.dump(pokemon).data
+    return custom_response(ser_pokemon, 200)
+
+@pokemon_api.route('/<str:name>', methods=['GET'])
+def get_pokemon_name(name):
+    '''
+    Get a single pokemon
+    '''
     pokemon = PokemonModel.get_one_pokemon(pokemonnumber)
     print(pokemon)
     if not pokemon:
@@ -26,22 +39,22 @@ def get_pokemon(pokemonnumber):
     ser_pokemon = pokemon_schema.dump(pokemon).data
     return custom_response(ser_pokemon, 200)
 
-@pokemon_api.route('/create', methods=['POST'])
-def create_pokemon():
+# @pokemon_api.route('/create', methods=['POST'])
+# def create_pokemon():
    
-    req_data = request.get_json()
-    data, error = pokemon_schema.load(req_data)
-    print('Printing data')
-    print(data, error)
-    if error:
-        return custom_response(error, 400)
+#     req_data = request.get_json()
+#     data, error = pokemon_schema.load(req_data)
+#     print('Printing data')
+#     print(data, error)
+#     if error:
+#         return custom_response(error, 400)
 
-    pokemon = PokemonModel(data)
-    print(pokemon)
-    pokemon.save()
+#     pokemon = PokemonModel(data)
+#     print(pokemon)
+#     pokemon.save()
 
-    ser_data = pokemon_schema.dump(pokemon).data
-    return custom_response(ser_data, 201)
+#     ser_data = pokemon_schema.dump(pokemon).data
+#     return custom_response(ser_data, 201)
 
 def custom_response(res, status_code):
     '''
