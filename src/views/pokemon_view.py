e0f9a68be18bd4f4fd4daf1bad621a871b9c1aa2
PRMcgill <mcgill.patrick2192@gmail.com>
2019-04-22 09:49:59 -0400

    added the routes to find the pokemon by number or name from the pokemonapi.

--- src/views/pokemon_view.py
+++ src/views/pokemon_view.py
@@ -1,5 +1,6 @@
 from flask import request, json, Response, Blueprint, g
 from ..models.pokemon import PokemonModel, PokemonSchema
+import pokebase as pb
 
 pokemon_api = Blueprint('pokemon', __name__)
 pokemon_schema = PokemonSchema()
@@ -13,18 +14,21 @@ def get_all():
     ser_pokemon = pokemon_schema.dump(pokemons, many=True).data
     return custom_response(ser_pokemon, 200)
 
-@pokemon_api.route('/<int:pokemonnumber>', methods=['GET'])
-def get_pokemon(pokemonnumber):
-    '''
-    Get a single pokemon
-    '''
-    pokemon = PokemonModel.get_one_pokemon(pokemonnumber)
-    print(pokemon)
-    if not pokemon:
-        return custom_response({'error': 'Pokemon not found'}, 404)
+@pokemon_api.route('/<int:number>', methods=['GET'])
+def get_pokemon(number):
+    new_pokemon = pb.pokemon(number)
+    new_pokemon_species = pb.pokemon_species(number)
+    pokemon_fte = new_pokemon_species.flavor_text_entries
+    for fte in pokemon_fte:
+            if fte.language.name =="en" and fte.version.name == "alpha-sapphire" or fte.language.name == "en" and fte.version.name == "ultra-sun":
+                    new_pokemon_description = fte.flavor_text
+    new_pokemon_name = new_pokemon.name
+    new_pokemon_number = new_pokemon.id
+    
+    new_pokemon_image = new_pokemon.sprites.front_default
 
-    ser_pokemon = pokemon_schema.dump(pokemon).data
-    return custom_response(ser_pokemon, 200)
+
+    return f'{new_pokemon_name},\n{new_pokemon_description},\n{new_pokemon_image},\n{new_pokemon_number}'
 
 @pokemon_api.route('/create', methods=['POST'])
 def create_pokemon():
@@ -42,6 +46,23 @@ def create_pokemon():
 
     ser_data = pokemon_schema.dump(pokemon).data
     return custom_response(ser_data, 201)
+
+@pokemon_api.route('/<string:name>', methods=['GET'])
+def pokemon_info(name):
+    new_pokemon = pb.pokemon(name)
+    new_pokemon_species = pb.pokemon_species(name)
+    pokemon_fte = new_pokemon_species.flavor_text_entries
+    for fte in pokemon_fte:
+            if fte.language.name =="en" and fte.version.name == "alpha-sapphire" or fte.language.name == "en" and fte.version.name == "ultra-sun":
+                    new_pokemon_description = fte.flavor_text
+    new_pokemon_name = new_pokemon.name
+    new_pokemon_number = new_pokemon.id
+    
+    new_pokemon_image = new_pokemon.sprites.front_default
+
+
+    return f'{new_pokemon_name},\n{new_pokemon_description},\n{new_pokemon_image},\n{new_pokemon_number}'
+
 
 def custom_response(res, status_code):
     '''
