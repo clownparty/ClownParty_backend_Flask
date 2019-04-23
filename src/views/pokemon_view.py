fbbdea9bd9f8d8782d42b9db24ec22552f18fcf8
PRMcgill <mcgill.patrick2192@gmail.com>
2019-04-23 14:22:05 -0400

    get pokemon by name

--- src/views/pokemon_view.py
+++ src/views/pokemon_view.py
@@ -14,12 +14,10 @@ def get_pokemon(ident):
 
 @pokemon_api.route('/name/<string:name>')
 def get_pokemon_name(name):
-    pokemon_data = request.get_json()
-    data =pokemon_schema.load(pokemon_data)
+    
     print(data, "---------------------------------")
 
     pokemon = PokemonModel.get_pokemon_by_name(name)
-    print(pokemon, "-----------------------------------------------------")
     if not pokemon:
         return custom_response({'error': 'no pokemon found'}, 404)
     poke_info = pokemon_schema.dump(pokemon).data
