6d8d35a034d8a77acfe95f7aa85b5a3e09cd9b54
PRMcgill <mcgill.patrick2192@gmail.com>
2019-04-23 11:42:51 -0400

    added the .first again and setting name=value

--- src/models/pokemon.py
+++ src/models/pokemon.py
@@ -61,7 +61,7 @@ class PokemonModel(db.Model):
         return PokemonModel.query.filter_by(pokemonnumber)
     @staticmethod
     def get_pokemon_by_name(name):
-        return PokemonModel.query.filter_by(name)
+        return PokemonModel.query.filter_by(name=value).first()
     
     @staticmethod
     def get_pokemon_by_type(pokemontype1):
