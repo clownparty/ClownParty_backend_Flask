4d075c588513d0aea006d3e9eeffa1edb79c0ca4
PRMcgill <mcgill.patrick2192@gmail.com>
2019-04-23 10:17:38 -0400

    testing print statement

--- src/views/user_view.py
+++ src/views/user_view.py
@@ -91,14 +91,15 @@ def login():
     if the user credentials are verified
     '''
     req_data = request.get_json()
+    print(req_data)
 
     data, error = user_schema.load(req_data, partial=True)
 
     if error:
         return custom_response(error, 400)
 
-    # if not data.get('email') or not data.get('password'):
-    #     return custom_response({'error': 'email and password ' + ' required to login'}, 404)
+    if not data.get('email') or not data.get('password'):
+        return custom_response({'error': 'email and password ' + ' required to login'}, 404)
 
     user = UserModel.get_user_by_email(data.get('email'))
     if not user:
