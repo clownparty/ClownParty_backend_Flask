d7c94a108e174ceeefae63eff7d878a02de9e120
connormullett <connormullett@gmail.com>
2019-04-23 13:46:14 -0400

    script working

--- pokebasetests.py
+++ pokebasetests.py
@@ -1,33 +1,34 @@
+#!/usr/bin/env python3
+
 import pokebase as pb
-import json
+import json, os, sys
 import requests
-URL = "https://pokemonteam-builder.herokuapp.com/api/v1/pokemon/create"
-headers = {
-    'populate_pokemon_token': 'testing',
-    'Content-Type': 'application/json'
-}
-        
+import psycopg2
+
+
+conn = psycopg2.connect(os.environ['DATABASE_URL'])
+cur = conn.cursor()
+sql = '''INSERT INTO pokemon(name, pokemonnumber, pokemondescription, pokemonimage)
+         VALUES(%s, %s, %s, %s) returning id;'''
+
 for i in range(1, 808):
+    # grab pokemon from library
     pokemon = pb.pokemon(i)
     species = pb.pokemon_species(i)
     description = ""
+    # check if always english
     for fte in species.flavor_text_entries:
         if fte.language.name =="en" and fte.version.name == "alpha-sapphire" or fte.language.name == "en" and fte.version.name == "ultra-sun":
             description = fte.flavor_text
-                        
-    data = {
-    'name': pokemon.name,
-    'pokemonnumber': pokemon.id,
-    'pokemondescription': description,
-    'pokemonimage': pokemon.sprites.front_default
-    }
 
-    data = json.dumps(data)
-    requests.post(URL, data, headers=headers)
-        
-   
+    name = pokemon.name,
+    pokemonnumber = pokemon.id,
+    pokemondescription = description,
+    pokemonimage = pokemon.sprites.front_default
 
+    cur.execute(sql, (name, pokemonnumber, pokemondescription, pokemonimage,))
+    conn.commit()
 
-
-
+cur.close()
+conn.close()
 
