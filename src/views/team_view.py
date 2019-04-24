b498d0acf5164d8f5e4c095116a199f9894a6389
PRMcgill <mcgill.patrick2192@gmail.com>
2019-04-24 15:46:22 -0400

    added the auth guard back in

--- src/views/team_view.py
+++ src/views/team_view.py
@@ -8,7 +8,7 @@ team_schema = TeamSchema()
 
 
 @team_api.route('/team/new', methods=['POST'])
-# @Auth.auth_required
+@Auth.auth_required
 def create():
     '''
     Create endpoint for team api
@@ -38,7 +38,7 @@ def create():
 
 
 @team_api.route('/team/edit/<int:id>', methods=['DELETE'])
-# @Auth.auth_required
+@Auth.auth_required
 def delete(id):
     '''
     Delete the team model
